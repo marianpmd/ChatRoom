3a1d75e0feeba30ec1cdc984785aac40aec1dcfd
Petrica Marian <marianpetrica221@gmail.com>
2020-08-27 15:21:28 +0300

    no message

--- ChatRoomServer/src/com/marian/Main.java
+++ ChatRoomServer/src/com/marian/Main.java
@@ -18,7 +18,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Server online...");
-        
         try{
         ServerSocket serverSocket = new ServerSocket(3999);
         while (true) {
